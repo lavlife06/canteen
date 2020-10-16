308df15950da68b26d3315466d0ea8eb172831d4
lavlife06 <lav7990192369@gmail.com>
2020-10-16 08:23:55 +0530

    some changes in forum

--- client/src/App.js
+++ client/src/App.js
@@ -34,12 +34,6 @@ function App() {
         {/* </section> */}
       </Fragment>
     </Router>
-import React from 'react';
-import Login from './components/auth/login';
-
-function App() {
-  return (
-    <Login/>
   );
 }
 
